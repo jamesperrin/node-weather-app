0487b70274ba5e842d4266ab87762d508be24333
James Perrin <countrydawgg@gmail.com>
2018-01-22 17:02:37 -0800

    (1)  Added DotEnv file to secure sensitive data. (2) Added weather api call.

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
+require('dotenv').config();
+const config = require('./config/config');
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+const request = require('request');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help().alias('help', 'h')
-  .argv;
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help().alias('help', 'h')
+//   .argv;
 
-// DEBUGGING CODE
-//console.log(argv);
+// // DEBUGGING CODE
+// //console.log(argv);
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+var url = config.apiUrl;
+var apiKey = config.apiKeyValue;
+const lat = '47.6587802';
+const lon = '-117.4260465';
+
+request({
+  url: `${url}/${apiKey}/${lat},${lon}`,
+  json: true
+}, (error, response, body) => {
+
+  if (!error && response.statusCode === 200) {
+    //DEBUGGING CODE
+    //console.log(body.currently);
+
+    console.log(`The current temperature is ${body.currently.temperature}.`);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.error('Unable to fetch weather.');
   }
 });
---
+++ config/config.js
@@ -0,0 +1,9 @@
+let apiUrl = process.env.API_URL;
+let apiKeyName = process.env.API_KEY_NAME;
+let apiKeyValue = process.env.API_KEY_VALUE;
+
+module.exports = {
+  apiUrl,
+  apiKeyName,
+  apiKeyValue
+};
